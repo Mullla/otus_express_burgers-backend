983104e6d914d104780e6a75b71af9effad7dbb9
Mars <diablo_13_06@mail.ru>
2023-01-23 01:30:20 +0300

    updates

--- index.js
+++ index.js
@@ -3,16 +3,14 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import router from './router.js';
 
-const PORT = 3001;
-const DB_URL =
-  'mongodb+srv://root:root1@cluster0.la4beut.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 async function startApp() {
   try {
     mongoose.set('strictQuery', false);
-    await mongoose.connect(DB_URL, {
+    await mongoose.connect(process.env.DB_URL, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
